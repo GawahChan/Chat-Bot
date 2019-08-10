f2ed7a928c366c203b8931880c7525bd3f16fcfb
Gawah Chan <gawahchan@gmail.com>
2019-08-10 15:27:56 +0100

    first chat sequence

--- chatBot.py
+++ chatBot.py
@@ -1,18 +1,27 @@
-said_hello = False
 
+class HelloProgram:
+        seq_num = 0
+        def run(self,message):
+            self.seq_num += 1
+            if self.seq_num == 1:
+                return "Hello, what is your name?"
+            elif self.seq_num == 2:
+                return "Hello {}, How old are you?".format(message)
+            elif self.seq_num == 3:
+                return "{}! That's so old!".format(message)
+            else: 
+                return "I don't want to talk to you anymore :("
     
 def get_response(message):
-    global said_hello
-    if not said_hello:
-        said_hello = True
-        return "Hello, what is your name?"
-    else:
-        return message + " That's a funny name"
+    pass
+
 
 def main():
     user_message = ""
+    program = HelloProgram()
     while True:
-        bot_response= get_response(user_message)
+        #bot_response= get_response(user_message)
+        bot_response = program.run(user_message)
         print(bot_response)
         user_message = input()
 
